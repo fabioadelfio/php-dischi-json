303e9b8ddb3b11fa9e2498f131533316ccc2974e
fabioadelfio <adelfiofabio07@gmail.com>
2025-07-30 16:17:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$dischi_text = file_get_contents('./dischi.json');
+$dischi = json_decode($dischi_text);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
